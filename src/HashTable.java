a3478aa4eb21463aaac89c075bd18e2f2c98fb54
deejanzen <djanzen@email.arizona.edu>
2016-04-26 17:14:22 -0700

    roughed out hashTable. 3/16 passed

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,25 +1,69 @@
+/**
+ * HashTable implements a basic ht using a sha-256 based hash() function
+ * @author Dustin Janzen
+ */
 public class HashTable{
     RussList [] hashTable;
 
-
+    /**
+     * HashTable constructor.
+     * @param sizeOfHashTable the size of the table
+     */
     public HashTable(int sizeOfHashTable){
         if (sizeOfHashTable < 2) throw new IllegalArgumentException();
         hashTable = new RussList[sizeOfHashTable];
-
-
     }
 
+    /**
+     * Inserts the key, value pair into a linked list collision strategy hash table at the end of LL.
+     * @param key the key
+     * @param value the matching value
+     */
     public void insert(String key, Object value){
+        int indexToAddObject =Proj06Main.genHash(key) % hashTable.length;
 
+        if (hashTable[indexToAddObject] == null) {
+            hashTable[indexToAddObject] = new RussList(key, value, null);
+            return;
+        }
+
+        RussList current = hashTable[indexToAddObject];
+        while(true){
+           if (current.getNext() == null) {
+               current.setNext(new RussList(key, value, null));
+               return;
+           }
+
+            current = current.getNext();
+        }
     }
 
+    /**
+     * Search for the key parameter. Returns Object, if found, null otherwise.
+     * @param key the key for which the value you seek.
+     * @return
+     */
     public Object search(String key){
-        return new Object();
+        int indexToGetObject =Proj06Main.genHash(key) % hashTable.length;
+        if (hashTable[indexToGetObject] == null) {
+            return null;
+        }
+        RussList current = hashTable[indexToGetObject];
+        while(true){
+            if (current.getNext() == null) {
+                if (current.getKey().equals(key)) return current.getValue();
+                else                              return null;
+            }
+            if (current.getKey().equals(key))     return current.getValue();
+            current = current.getNext();
+        }
+
     }
 
+    /**
+     * debug println
+     */
     public void debugDump(){
-
+        System.out.println("debugDump was called");
     }
-
-
 }//end HashTable
